fffc054d9718fe1b0a0023be6f8595177c22997a
KoudouMizuki <k.mizuki0706@gmail.com>
2023-06-13 11:07:33 +0000

    PHP if文学習

---
+++ if.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <mata charset = "utf-8">
+    <title>pactice PHP</title>
+    <link rel = "stylesheet" type = "text/css" href = "stylesheet.css">
+  </head>
+  
+  <body>
+    <?php
+      $x = 99 * 99;
+      $y = 77 * 77;
+      
+      if($x > 9800) {
+        echo "変数xは9800より大きいです。";
+      }
+      
+      if($y > 6000) {
+        echo "変数yは6000より大きいです。";
+      }
+    ?>
+  </body>
+</html>
