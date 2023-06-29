5b3c306073c5ddca3901e090974f1b5d61fa0a7f
KoudouMizuki <k.mizuki0706@gmail.com>
2023-06-29 11:12:01 +0000

    PHP学習　応用

--- study2_php/study1.7.php
+++ study2_php/study1.7.php
@@ -6,12 +6,20 @@
   );
   
   $totalPrice = 0;
+  $maxPrice = 0;
+  $maxPriceMenuName = "";
   foreach($menus as $menu) {
     $price = $menu['price'];
     $name = $menu['name'];
     echo "{$name}は{$price}円です";
     echo '<br>';
     $totalPrice += $price;
+    if($maxPrice < $price) {
+      $maxPrice = $price;
+      $maxPriceMenuName = $name;
+    }
   }
   echo "合計金額は{$totalprice}円です";
+  echo '<br>';
+  echo "{$maxPriceMenuName}が最高価格で{$maxPrice}円です";
 ?>
