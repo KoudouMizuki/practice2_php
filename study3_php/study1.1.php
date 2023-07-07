2abded648cd31cd99fa15e0bd8173956cb478eb4
KoudouMizuki <k.mizuki0706@gmail.com>
2023-07-07 11:46:03 +0000

    PHP学習

--- study3_php/study1.1.php
+++ study3_php/study1.1.php
@@ -1,17 +1,20 @@
 <?php
   class Menu{
     public $name;
+    public function __construct($name) {
+      $this -> name = $name;
+    }
     public function hello() {
       echo "私は{$this -> name}です";
     }
   }
+  $juice = new Menu('JUICE');
+  $coffee = new Menu('COFFEE');
+  $curry = new Menu('CURRY');
+  $pasta = new Menu('PASTA');
   
-  $curry = new Menu();
-  $pasta = new Menu();
-  $curry -> name = "CURRY";
-  $pasta -> name = "PASTA";
-  
-  echo $curry -> hello();
-  echo '<br>';
-  echo $pasta -> hello();
 ?>
+<p><?php echo $juice -> name ?></p>
+<p><?php echo $coffee -> name </p>
+<p><?php echo $curry -> name ?></p>
+<p><?php echo $pasta -> name ?></p>
